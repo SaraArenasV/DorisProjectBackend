0dba0cca5bb082586351e330ceeda3b40890b0db
attitude011 <rafaelgoran@gmail.com>
2022-04-01 17:54:20 -0300

    Validation method created+logic implementation

--- src/main/java/micro/doris/controller/ProductApiController.java
+++ src/main/java/micro/doris/controller/ProductApiController.java
@@ -1,11 +1,12 @@
 package micro.doris.controller;
 
-
-
 import micro.doris.entity.Product;
 import micro.doris.services.IProductService;
 import micro.doris.services.Impl.ProductService;
 import micro.doris.viewmodel.ProductModel;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,35 +20,41 @@ import org.springframework.web.bind.annotation.*;
 
 public class ProductApiController {
 
-    @Autowired
-    private IProductService service;
+	@Autowired
+	private IProductService service;
 
+	@GetMapping("/getProduct/{sku}")
+	public ResponseEntity<?> findById(@PathVariable(name = "sku") String sku) {
+		Product product = service.findProductBySku(sku);
+		if (product != null)
+			return new ResponseEntity<>(product, HttpStatus.OK);
+		else
+			return new ResponseEntity<>("El sku no existe", HttpStatus.OK);
+	}
 
-    @GetMapping("/getProduct/{sku}")
-    public ResponseEntity<?>  findById(@PathVariable(name="sku") String sku) {
-        Product product = service.findProductBySku(sku);
-        if (product!=null)
-        return  new ResponseEntity<> (product, HttpStatus.OK);
-        else
-            return new ResponseEntity<>("El sku no existe", HttpStatus.OK);
-    }
+	@GetMapping("/getProductByIdList/{id}")
+	public ResponseEntity<?> findByIdsl(@PathVariable(name = "id") Integer id) {
+		List<Product> product = service.findProductById(id);
+		if (product.size() > 0)
+			return new ResponseEntity<>(product, HttpStatus.OK);
+		else
+			return new ResponseEntity<>("Product not have assigned requested category", HttpStatus.OK);
+	}
 
-    @PostMapping("/saveProduct")
-  public ResponseEntity<?> insert(@RequestBody ProductModel product) {
-        Product productNew = service.findProductBySku(product.getSku());
-        if (productNew!=null){
-            return new ResponseEntity<>("El sku ya existe", HttpStatus.NOT_FOUND.OK);
-        }else {
-            return new ResponseEntity<>(service.save(product), HttpStatus.OK);
-        }
+	@PostMapping("/saveProduct")
+	public ResponseEntity<?> insert(@RequestBody ProductModel product) {
+		Product productNew = service.findProductBySku(product.getSku());
+		if (productNew != null) {
+			return new ResponseEntity<>("El sku ya existe", HttpStatus.NOT_FOUND.OK);
+		} else {
+			return new ResponseEntity<>(service.save(product), HttpStatus.OK);
+		}
 
-    }
-//
+	}
 
-    @PostMapping("/addproduct")
-    public ResponseEntity<Product> addProduct(@RequestParam String sku, Integer cantidad) {
-        return new ResponseEntity<>(service.addStock(sku,cantidad), HttpStatus.OK);
-    }
-
+	@PostMapping("/addproduct")
+	public ResponseEntity<Product> addProduct(@RequestParam String sku, Integer cantidad) {
+		return new ResponseEntity<>(service.addStock(sku, cantidad), HttpStatus.OK);
+	}
 
 }
