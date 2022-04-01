369751cf213994c606972ec832ae2be133222cd0
Ruben Barrera <rbarrera@bmsoluciones.cl>
2022-04-01 00:26:17 -0300

    Update ProductRepository.java

--- src/main/java/micro/doris/repository/ProductRepository.java
+++ src/main/java/micro/doris/repository/ProductRepository.java
@@ -1,6 +1,5 @@
 package micro.doris.repository;
 
-import micro.doris.entity.CategoryEntityJpa;
 import micro.doris.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
