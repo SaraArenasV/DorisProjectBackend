b4237d7a7b770978b5376eb738e3be0975fd3b9c
Ruben Barrera <rbarrera@bmsoluciones.cl>
2022-04-01 11:40:50 -0300

    Update ProductApiController.java

--- src/main/java/micro/doris/controller/ProductApiController.java
+++ src/main/java/micro/doris/controller/ProductApiController.java
@@ -9,11 +9,12 @@ import micro.doris.viewmodel.ProductModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Validated
-@RestController
+@Controller
 @RequestMapping("/api")
 
 public class ProductApiController {
@@ -41,7 +42,7 @@ public class ProductApiController {
         }
 
     }
-
+//
 
     @PostMapping("/addproduct")
     public ResponseEntity<Product> addProduct(@RequestParam String sku, Integer cantidad) {
